257155f2b14b24a94571fbba2f1f3dfe9ffff144
Zshaikh <zshaikh@hexalytics.com>
2025-12-24 20:07:50 +0530

    Adding workflow and updating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './', // Use relative paths for assets to support any deployment subpath
+//   build: {
+//     outDir: 'dist',
+//     sourcemap: false
+//   },
+//   server: {
+//     port: 3000
+//   }
+// });
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: './', // Use relative paths for assets to support any deployment subpath
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  server: {
-    port: 3000
-  }
-});
+})
